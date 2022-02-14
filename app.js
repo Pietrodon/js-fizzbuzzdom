04b8a4e78c49f4a304ca1aca66c3f87d2d5fd8e5
Pietrodon <donvitopietro00@gmail.com>
2022-02-14 17:56:04 +0100

    rifiniture

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
 // richiamo il container per gli elementi
 const contenitore = document.querySelector('.container')
-
-
-
-
-
 // ciclo for per stampare numeri da 1 a 100
 for (let i = 1; i <=100; i++) {
     // creazione elemento html e aggiunta classe
@@ -12,14 +7,16 @@ for (let i = 1; i <=100; i++) {
     div.append([i])
     div.classList.add('box')
     console.log(div) 
-
-    // verifica per che numeri è divisibile 
+// verifica per che numeri è divisibile e aggiunta classe relativa 
     if(i % 3 === 0 && i % 5 ===0){
-        console.log(i,'fizzbuzz');
+        // console.log(i,'fizzbuzz')
+        div.classList.add('fizzbuzz')
     } else if(i % 3 === 0){
-        console.log(i,'fizz')
+        // console.log(i,'fizz')
+        div.classList.add('fizz')
     } else  if(i % 5 ===0) {
-        console.log(i,'buzz')
+        // console.log(i,'buzz')
+        div.classList.add('buzz')
     } else{
         console.log(i)
     }
