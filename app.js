217b3ce60a3a25bc44abdd62dc332c08debfe5f8
Pietrodon <donvitopietro00@gmail.com>
2022-02-14 17:14:45 +0100

    ciclo for per capire che classe assegnare al box

---
+++ app.js
@@ -0,0 +1,17 @@
+let boxHtml = document.getElementsByClassName('box')
+
+for (let i = 0; i <=100; i++) {
+    
+    if(i % 3 === 0 && i % 5 ===0){
+        console.log(i,'fizzbuzz');
+    } else if(i % 3 === 0){
+        console.log(i,'fizz')
+    } else  if(i % 5 ===0) {
+        console.log(i,'buzz')
+    } else{
+        console.log(i)
+    }
+
+
+
+}
